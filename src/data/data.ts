af441db4cee604fece1f2cb57981af01a24d45dd
Fabián Vargas <fabidev18@gmail.com>
2025-04-08 13:35:32 -0600

    Update repertoire with real songs and artists

--- src/data/data.ts
+++ src/data/data.ts
@@ -5,48 +5,62 @@ export const events = [
 
 export const repertoire = {
   salsa: [
-    { title: "Llorarás", artist: "Oscar D'León" },
-    { title: "Pedro Navaja", artist: "Rubén Blades" },
-    { title: "Vivir Mi Vida", artist: "Marc Anthony" },
-    { title: "Idilio", artist: "Willie Colón" },
-    { title: "Mi Gente", artist: "Héctor Lavoe" },
+    { title: "Deseandote", artist: "Frankie Ruiz" },
+    { title: "Sin salsa no hay paraiso", artist: "El Gran Combo de Puerto Rico" },
+    { title: "Que locura enamorarme de ti", artist: "Eddie Santiago" },
+    { title: "Casi te envidio", artist: "Andy Montañez" },
+    { title: "La noche mas linda", artist: "Adalberto Santiago" },
+    { title: "Tu cariñito", artist: "Luisito Ayala y La Puerto Rican Power" },
+    { title: "Como te hago entender", artist: "Roberto Roena" },
+    { title: "De mi enamorate", artist: "Zarella" },
   ],
   merengue: [
-    { title: "Suavemente", artist: "Elvis Crespo" },
-    { title: "El Africano", artist: "Wilfrido Vargas" },
-    { title: "La Bilirrubina", artist: "Juan Luis Guerra" },
-    { title: "El Baile del Perrito", artist: "Wilfrido Vargas" },
-    { title: "La Vaca", artist: "Mala Fe" },
+    { title: "Yo me siento enamorado", artist: "Josie Esteban" },
+    { title: "Que loco", artist: "Grupo Mania" },
+    { title: "Agua de coco", artist: "Jossie Esteban" },
+    { title: "Morena ven", artist: "Los Hermanos Rosario" },
+    { title: "Es mentiroso", artist: "Olga Tañón" },
+    { title: "Rompecintura", artist: "Los Hermanos Rosario" },
+    { title: "Nuestra canción", artist: "Elvis Crespo" },
+    { title: "Nadie se muere", artist: "La Makina" }
   ],
   cumbia: [
-    { title: "Oye Mi Amor", artist: "Los Ángeles Azules" },
-    { title: "La Pollera Colorá", artist: "Pedro Salcedo" },
-    { title: "El Pescador", artist: "Totó la Momposina" },
-    { title: "Mi Cucu", artist: "La Sonora Dinamita" },
-    { title: "Que Nadie Sepa Mi Sufrir", artist: "Los Mirlos" },
+    { title: "Que bello", artist: "La Sonora Dinamita" },
+    { title: "Nunca es suficiente", artist: "Los Ángeles Azules" },
+    { title: "Pedacito de mi vida", artist: "La Sonora Santanera, Matisse" },
+    { title: "Golpe con golpe", artist: "Pastor Lopez" },
+    { title: "Cumbia caletera", artist: "Billo's Caracas Boys" },
+    { title: "Plegaria vallenata", artist: "Pastor Lopez" },
+    { title: "Magdalena", artist: "Rigo Dominguez y Su Grupo Audaz" },
+    { title: "Talento de TV", artist: "Willie Colón" },
+    { title: "Encontré la cadenita", artist: "Las Sonora Dinamita" },
+    { title: "Juana la cubana", artist: "Fito Olivares" },
+    { title: "Se me perdió la cadenita", artist: "Las Sonora Dinamita" },
+    { title: "Caleñas", artist: "Pastor Lopez" },
+    { title: "Tiene espinas el rosal", artist: "Grupo Cañaveral" },
+    { title: "La parabólica", artist: "La Sonora Dinamita" },
+    { title: "Mil horas", artist: "La Sonora Dinamita" },
+    { title: "Amaneciendo", artist: "Adolfo Echeverria" }
   ],
   bolero: [
-    { title: "Bésame Mucho", artist: "Lucho Gatica" },
-    { title: "Sabor a Mí", artist: "Los Panchos" },
-    { title: "Sin Ti", artist: "Los Tres Ases" },
-    { title: "Perfidia", artist: "Los Tres Diamantes" },
-    { title: "Amor Eterno", artist: "Rocío Dúrcal" },
+    { title: "Perfume de gardenias", artist: "La Sonora Santanera" },
+    { title: "Conversación en tiempo de bolero", artist: "Vicentico Valdés" },
+    { title: "Urge", artist: "La Sonora Santanera" },
+    { title: "Mi buen amor", artist: "Gloria Estefan" }
   ],
   bachata: [
-    { title: "Propuesta Indecente", artist: "Romeo Santos" },
-    { title: "Obsesión", artist: "Aventura" },
-    { title: "Por Un Segundo", artist: "Aventura" },
-    { title: "Darte Un Beso", artist: "Prince Royce" },
-    { title: "Stand By Me", artist: "Prince Royce" },
+    { title: "Hoy lo vi pasar", artist: "?" },
+    { title: "Quizás sí, quizás no", artist: "Los Toros Band" },
+    { title: "La bachata", artist: "Manuel Turizo" },
+    { title: "Bailando dos corazones", artist: "Chayanne" },
   ],
   "paso-doble": [
-    { title: "España Cañí", artist: "Pascual Marquina" },
-    { title: "Gallito", artist: "José Franco" },
-    { title: "El Gato Montés", artist: "Manuel Penella" },
-    { title: "Suspiros de España", artist: "Antonio Álvarez Alonso" },
-    { title: "La Entrada de los Gladiadores", artist: "Julius Fučík" },
+    { title: "Bomboro", artist: "La Sonora Santanera" },
+    { title: "Pena negra", artist: "La Sonora Santanera" },
+    { title: "El beso", artist: "Los Churumbeles de España" }
   ]
-}
+};
+
 
 export const genres = [
   { id: "salsa", name: "Salsa" },
@@ -62,79 +76,66 @@ export const members = [
     name: "Julio Salas",
     instrument: "Director y Piano",
     image: "/integrantes/julio-salas_landscape.jpg",
-    bio: "Maestro del piano con más de 20 años de experiencia en la música latina. Julio lidera la orquesta con pasión y precisión.",
   },
   {
     name: "Aylin Salas",
     instrument: "Voz",
     image: "/integrantes/aylin-salas.jpg",
-    bio: "Biografía de "
   },
   {
     name: "Keylin Mora",
     instrument: "Voz",
     image: "/placeholder.svg?height=400&width=300",
-    bio: "Biografía de Keylin Mora"
   },
   {
     name: "Anthony Lumbi",
     instrument: "Voz",
     image: "/integrantes/anthony-lumbi_landscape.jpg",
-    bio: "Biografía de Anthony Lumbi"
   },
   {
     name: "Tomás Vásquez",
     instrument: "Bajo",
     image: "/integrantes/tomas-vasquez.PNG",
-    bio: "Biografía de Tomás Vásquez"
   },
   {
     name: "Amy Pérez",
     instrument: "Sax Alto",
     image: "/integrantes/amy-perez_landscape.jpg",
-    bio: "Biografía de Amy Pérez",
   },
   {
     name: "Fabián Vargas",
     instrument: "Sax Alto",
     image: "/integrantes/fabian-vargas.jpg",
-    bio: "Biografía de Fabián Vargas",
   },
   {
     name: "Erick Piedra",
     instrument: "Sax Tenor",
     image: "/integrantes/erick-piedra.jpg",
-    bio: "Biografía de Erick Piedra",
   },
   {
     name: "Mauricio (Coco)",
     instrument: "Trompeta",
     image: "/integrantes/mauricio-coco_landscape.jpg",
-    bio: "Biografía de Mauricio (Coco)",
   },
   {
     name: "Johan Román",
     instrument: "Trombón",
     image: "/integrantes/johan-roman_landscape.jpg",
-    bio: "Biografía de Johan Román",
   },
   {
     name: "Joseph Barquero",
     instrument: "Timbales",
     image: "/integrantes/joseph-barquero_landscape.jpg",
-    bio: "Biografía de Joseph Barquero",
   },
   {
     name: "Kike Obando",
     instrument: "Congas",
     image: "/integrantes/kike-obando_landscape.png",
-    bio: "Biografía de Kike Obando"
   },
   {
     name: "Jairo Huertas",
     instrument: "Percusión Menor",
     image: "/integrantes/jairo-huertas_landscape.jpg",
-    bio: "Biografía de Jairo Huertas"
   }
 ];
 
