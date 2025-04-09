1b1ac0a353ddd5aaa5a3e95fc3fedecc10b56473
Fabián Vargas <fabidev18@gmail.com>
2025-04-08 21:55:45 -0600

    Add Vercel Analytics to Layout component for improved tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,19 @@ import icon from 'astro-icon';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon(), react()]
+  integrations: [icon(), react()],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
